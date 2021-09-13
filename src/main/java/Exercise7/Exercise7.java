2b3818cdb6b8b2139fb60e359506cdfd0d442846
BiggyBear19 <bernardthebear19@gmail.com>
2021-09-13 15:36:34 -0400

    Initial commit

---
+++ src/main/java/Exercise7/Exercise7.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+package Exercise7;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args){
+
+        Scanner data = new Scanner(System.in);
+
+        System.out.print("PLease enter the length of the room in feet:");
+
+        String STRlength = data.nextLine();
+
+        System.out.print("Please enter the width of the room:");
+
+        String STRwidth = data.nextLine();
+
+        var length = Integer.parseInt(STRlength, 10);
+
+        var width = Integer.parseInt(STRwidth, 10);
+
+        int area = length * width;
+
+        System.out.println("The area is");
+
+        System.out.printf("%d square feet\n", area);
+
+        double m_to_fconvert = 0.09290304;
+
+        double m2 = area * m_to_fconvert;
+
+        System.out.printf("%f square meters", m2);
+
+
+
+    }
+
+}
